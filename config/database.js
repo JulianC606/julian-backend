59ee820c6eebbb5b127e3dd66f56a676d172a538
Julian Cipagauta <Julin Cipagauta>
2021-07-04 23:46:40 -0500

    small fix: postgres

--- config/database.js
+++ config/database.js
@@ -10,7 +10,7 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env('DATABASE_SSL', false)
+        ssl: { rejectUnauthorized: false }
       },
       options: {
         useNullAsDefault: true
